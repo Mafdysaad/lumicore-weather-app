2b42079b1a2e664c1691975863e464eae357e885
mafdysaad <mafdysaad366@gmail.com>
2026-06-18 22:22:41 +0300

    refact the code

--- lib/core/utils/fontstyle.dart
+++ lib/core/utils/fontstyle.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Fontstyle {
-  static TextStyle spacegrotesk_semiBold = const TextStyle(
+  static TextStyle spacegrotesk_semiBold = TextStyle(
     fontFamily: 'spaceGrotesk',
-    fontSize: 30,
     fontWeight: FontWeight.w600,
     letterSpacing: 1.5,
     color: Color(0xff31507F),
--- lib/core/utils/responsive_size.dart
+++ lib/core/utils/responsive_size.dart
@@ -1,15 +1,12 @@
-double responsive_size({
-  required double fontSize,
-  required double screenWidth,
-}) {
-  double scalfactor = get_scalfactor(screenWidth);
+double responsivesize({required double fontSize, required double screenWidth}) {
+  double scalfactor = getscalfactor(screenWidth);
   double responsiveSize = fontSize * scalfactor;
   double lowerLimit = fontSize * .6;
   double upperLimit = fontSize * 1.2;
   return responsiveSize.clamp(lowerLimit, upperLimit);
 }
 
-double get_scalfactor(double width) {
+double getscalfactor(double width) {
   // PlatformDispatcher dispatcher = PlatformDispatcher.instance;
   // double physicalwidth = dispatcher.views.first.physicalSize.width;
   // double devicepixelratio = dispatcher.views.first.devicePixelRatio;

af5d2aff23031eaf84095410e5351d87eb55a680
mafdysaad <mafdysaad366@gmail.com>
2026-06-18 22:18:33 +0300

    add responsive font size and image assets

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String resourceImagesMoon = "assets/images/moon.svg";
+
+  static const String resourceImagesSun = "assets/images/sun.svg";
+}
+
---
+++ lib/core/utils/responsive_size.dart
@@ -0,0 +1,24 @@
+double responsive_size({
+  required double fontSize,
+  required double screenWidth,
+}) {
+  double scalfactor = get_scalfactor(screenWidth);
+  double responsiveSize = fontSize * scalfactor;
+  double lowerLimit = fontSize * .6;
+  double upperLimit = fontSize * 1.2;
+  return responsiveSize.clamp(lowerLimit, upperLimit);
+}
+
+double get_scalfactor(double width) {
+  // PlatformDispatcher dispatcher = PlatformDispatcher.instance;
+  // double physicalwidth = dispatcher.views.first.physicalSize.width;
+  // double devicepixelratio = dispatcher.views.first.devicePixelRatio;
+  // double width_of_screen = physicalwidth / devicepixelratio;
+  if (width < 800) {
+    return width / 550;
+  } else if (width < 1300) {
+    return width / 1000;
+  } else {
+    return width / 1800;
+  }
+}

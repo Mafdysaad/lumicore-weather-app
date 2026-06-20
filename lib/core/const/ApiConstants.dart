be7c9ac8dd46e16898e656b76e9c4c8a8b51745d
mafdysaad <mafdysaad366@gmail.com>
2026-06-20 21:07:09 +0300

    add flutter_dotenv package

--- lib/core/const/ApiConstants.dart
+++ lib/core/const/ApiConstants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Apiconstants {
-  static String? _token = dotenv.env["WEATHER_API_KEY"];
+  static final String? token = dotenv.env["WEATHER_API_KEY"];
   static const String baseUrl =
       'https://api.openweathermap.org/data/2.5/weather?';
 }
